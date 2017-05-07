4b8c683b39d2ab3bc20196877352f19a4dc19206
Rafael Maia <rafael7maia@gmail.com>
2017-05-07 14:54:58 -0300

    Web Curriculum Vitae Version 1.1 require file

---
+++ maia.php
@@ -0,0 +1,36 @@
+<?php
+$nome = 'Rafael Maia';
+$profissao = 'Manager, Consultant, Developer and Design';
+$especialidade = 'Management, Security, BPM, PHP, Javascrip, CSS, HTML5';
+
+$contato = ['Email' => 'mailto:rafael7maia@gmail.com',
+    'Twitter' => 'http://twitter.com/rafael7maia',
+    'Linkedin' => 'http://linkedin.com/in/rafaelmaia',
+    'Github' => 'http://github.com/rafael7maia'];
+
+$formacao = [['inicio' => 2007,
+    'termino' => 2010,
+    'instituicao' => 'Universidade Estácio de Sá',
+    'ocupacao' => 'Tecnologo em Redes de Computadores'],
+    ['inicio' => '2006',
+        'termino' => '2008',
+        'instituicao' => 'Faculdade de Tecnologia do Nordeste',
+        'ocupacao' => 'Gerência de Redes de Computadores']];
+
+$experiencia = [['inicio' => 2016,
+    'termino' => FALSE,
+    'instituicao' => 'Brazilian Army',
+    'ocupacao' => 'IT Infraestruture Manager'],
+    ['inicio' => '2016',
+        'termino' => FALSE,
+        'instituicao' => 'Faculdades Cearenses',
+        'ocupacao' => 'Professor']];
+
+$portfolio = [['titulo' => 'Sucesu Ceará',
+    'url' => 'http://www.sucesuce.org.br',
+    'ano' => 2011],
+    ['titulo' => 'Associação de Peritos em Computação Forense',
+        'url' => 'http://www.apecof.org.br',
+        'ano' => 2015]];
+
+$profhabil = ['html' => 60, 'css' => 50, 'javascript' => 60, 'php' => 50];?>
